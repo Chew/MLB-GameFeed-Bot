d8eaf258fdc57d8ee657d26af120b2362242914d
Chew <chew@chew.pw>
2022-10-17 19:24:39 -0500

    Update activity and upgrade to JDA 5.0.0-alpha.21

--- src/main/java/pw/chew/mlb/MLBBot.java
+++ src/main/java/pw/chew/mlb/MLBBot.java
@@ -42,7 +42,7 @@ public class MLBBot {
         CommandClientBuilder client = new CommandClientBuilder();
 
         // Set the client settings
-        client.useDefaultGame();
+        client.setActivity(Activity.competing("the World Series"));
         client.setOwnerId("476488167042580481");
         client.setPrefix("woody!");
 
--- src/main/java/pw/chew/mlb/listeners/JDAListeners.java
+++ src/main/java/pw/chew/mlb/listeners/JDAListeners.java
@@ -1,7 +1,7 @@
 package pw.chew.mlb.listeners;
 
-import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.ShutdownEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.events.session.ShutdownEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.mapdb.DB;
